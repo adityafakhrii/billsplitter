2ed8b2e7b40f0113f7829fc6de2b64efaf20ff09
Aditya Fakhri Riansyah <adityafakhri03@gmail.com>
2025-07-02 07:50:33 +0000

    nominal harga jangan ada dibulatkan, tulis aja semuanya full

--- src/components/bill-splitter.tsx
+++ src/components/bill-splitter.tsx
@@ -44,7 +44,6 @@ export function BillSplitter() {
     return new Intl.NumberFormat("id-ID", {
       style: "currency",
       currency: "IDR",
-      minimumFractionDigits: 0,
     }).format(amount);
   };
 
